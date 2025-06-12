372d600a3dfda230b0b9e0f237f043d1b182fefc
SarangTandel5112 <sarangtandel5112@gmail.com>
2025-06-13 00:57:40 +0530

    Fix

--- src/components/common/TitleDescription.tsx
+++ src/components/common/TitleDescription.tsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 function TitleDescription({
     title,
     description,
@@ -8,19 +6,14 @@ function TitleDescription({
     description: string;
 }) {
     return (
-        <motion.div
-            className="flex flex-col gap-2"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-        >
+        <div className="flex flex-col gap-2">
             <h1 className="text-heading1 text-navy-blue font-semibold tracking-[0.608px]">
                 {title}
             </h1>
             <p className="text-navy-blue-400 text-description1 leading-6 tracking-[0.2px] capitalize">
                 {description}
             </p>
-        </motion.div>
+        </div>
     );
 }
 
