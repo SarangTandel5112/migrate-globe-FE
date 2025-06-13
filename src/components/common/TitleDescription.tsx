e4390e5df12d75cf10d0d97bcd9a042e633b80d2
SarangTandel5112 <sarangtandel5112@gmail.com>
2025-06-13 11:41:22 +0530

    visa page anmiation

--- src/components/common/TitleDescription.tsx
+++ src/components/common/TitleDescription.tsx
@@ -1,3 +1,5 @@
+import { motion, easeOut } from "framer-motion";
+
 function TitleDescription({
     title,
     description,
@@ -6,14 +8,19 @@ function TitleDescription({
     description: string;
 }) {
     return (
-        <div className="flex flex-col gap-2">
+        <motion.div
+            className="flex flex-col gap-2"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: easeOut }}
+        >
             <h1 className="text-heading1 text-navy-blue font-semibold tracking-[0.608px]">
                 {title}
             </h1>
             <p className="text-navy-blue-400 text-description1 leading-6 tracking-[0.2px] capitalize">
                 {description}
             </p>
-        </div>
+        </motion.div>
     );
 }
 
