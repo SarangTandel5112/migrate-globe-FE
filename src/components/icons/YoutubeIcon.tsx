3f8f7bf95d277bf4deab885948ba4d822800a9bf
Sm <smit.tank09@gmail.com>
2025-06-28 14:48:59 +0530

    fix type issue

--- src/components/icons/YoutubeIcon.tsx
+++ src/components/icons/YoutubeIcon.tsx
@@ -1,5 +1,4 @@
-import * as React from "react"
-const YoutubeIcon = (props: any) => (
+const YoutubeIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={27}
